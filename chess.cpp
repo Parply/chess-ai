f1a716afd734d80658e91e8e02729f67130870e9
Parply <alex_pinches@btinternet.com>
2020-10-14 16:16:01 +0100

    game class

--- chess.cpp
+++ chess.cpp
@@ -1,4 +1,7 @@
 #pragma once
+#include <vector>
+#include <string>
+
 
 class chess
 {
@@ -48,6 +51,14 @@ class chess
       			char chBefore;
       			char chAfter;
    		};
+
+		 struct IntendedMove
+   		{
+      			char chPiece;
+      			Position from;
+      			Position to;
+   		};
+
 		const char initial_board[8][8] =
    		{
       		// This represents the pieces on the board.
@@ -63,4 +74,54 @@ class chess
       		{ 'p',  'p',  'p',  'p',  'p',  'p',  'p',  'p' },
       		{ 'r',  'n',  'b',  'q',  'k',  'b',  'n',  'r' },
    		};
+};
+
+class game : chess
+{
+	public:
+
+	game();
+	~game();
+   	void movePiece( Position present, Position future, chess::EnPassant* S_enPassant, chess::Castling* S_castling, chess::Promotion* S_promotion );
+
+	char getPieceAtPosition(int iRow, int iColumn);
+	char getPieceAtPosition(Position pos);
+	char getPiece_considerMove( int iRow, int iColumn, IntendedMove* intended_move = nullptr );
+
+	bool isReachable( int iRow, int iColumn, int iColour );
+
+   	bool isSquareOccupied( int iRow, int iColumn );
+
+   	bool isPathFree( Position startingPos, Position finishingPos, int iDirection ); 
+
+   	bool canBeBlocked( Position startingPos, Position finishinPos, int iDirection );
+
+   	bool isCheckMate();
+
+   	bool isKingInCheck( int iColour, IntendedMove* intended_move = nullptr );
+
+   	bool playerKingInCheck( IntendedMove* intended_move = nullptr );
+
+   	bool wouldKingBeInCheck( char chPiece, Position present, Position future, EnPassant* S_enPassant );
+
+   	Position findKing( int iColour );
+
+	void parseMove( std::string move, Position* pFrom, Position* pTo, char* chPromoted = nullptr );
+
+   	void logMove( std::string &to_record );
+
+	struct Round
+   	{
+		std::string white_move;
+		std::string black_move;
+   	};
+
+	std::vector<char> white_captured;
+   	std::vector<char> black_captured;
+
+	private:
+
+   	// Represent the pieces in the board
+   	char board[8][8];
+
 };
